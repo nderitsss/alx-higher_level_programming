a72459e3475f52778f4871d9ed7260299d09e4a5
root <imaninderitu14@gmail.com>
2022-09-22 15:31:07 +0000

    Third Task : print arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    n = len(sys.argv) - 1
+    if(n == 0):
+        print("{} arguments.".format(n))
+    elif(n == 1):
+        print("{} argument".format(n))
+    else:
+        print("{} arguments:".format(n))
+    for i in range(0, n):
+        print("{}: {}".format((i + 1), sys.argv[i + 1]))
