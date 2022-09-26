494f1779ccab2b7665354506f2b8f9d6447eed96
root <imaninderitu14@gmail.com>
2022-09-26 15:27:36 +0000

    Task 8: prints len and first character

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    for i in range(0, 2):
-        if tuple_a[i] == None:
-            tuple_a[i] = 0
-        elif tuple_b[i] == None:
-            tuple_b[i] = 0
-        else :
-            tuple_a[i] *= tuple_a[i]
-            tuple_b[i] *= tuple_b[i]
-    return ((tuple_a[0] + tuple_b[0]),(tuple_a[1] + tuple_b[1]))
+    return (tuple_a[0] + tuple_b[0]),(tuple_a[1] + tuple_b[1])
---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    n =len(sentence)
+    p = sentence[0]
+    return n,p
