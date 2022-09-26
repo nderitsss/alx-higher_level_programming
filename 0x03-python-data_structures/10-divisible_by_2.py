9b9483fdf6bb6203f84e04420d1a81e412da2cf3
root <imaninderitu14@gmail.com>
2022-09-26 15:11:40 +0000

    Fixing checkers

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     b = []
     for i in my_list:
         if i % 2 == 0:
-           b.append(True)
+            b.append(True)
         else:
-           b.append(False)
+            b.append(False)
     return b
