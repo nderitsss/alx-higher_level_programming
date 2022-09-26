343045c65eec930740fdb2b0f4bdb942499100b8
root <imaninderitu14@gmail.com>
2022-09-26 10:01:48 +0000

    Print without c

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx > len(my_list):
+        return my_list
+    b = my_list
+    for i in range(0, len(b)):
+        if i == idx:
+            b[i] = element
+            return b
---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    for i in range(0, len(my_string)):
+        if my_string[i] == 'C' or my_string[i] == 'c':
+            continue
+        else:
+            print("{}".format(my_string[i]), end="")
+    return ''
