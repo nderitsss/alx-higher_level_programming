58c5ee8513ea20c19277385b2fdb6366ca58bc4b
root <imaninderitu14@gmail.com>
2022-09-26 15:32:00 +0000

    Fixing pep8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    n =len(sentence)
+    n = len(sentence)
     if n == 0:
         p = None
     p = sentence[0]
-    return n,p
+    return n, p
